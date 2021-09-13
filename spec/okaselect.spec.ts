579dedd7be5e4a4083b4501dce52530eec347133
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-09-13 21:20:30 +0900

    feat: craete selectable

--- spec/okaselect.spec.ts
+++ spec/okaselect.spec.ts
@@ -1,7 +1,40 @@
-import { hello } from '../src/okaselect'
+import { useSelectable } from '../src/okaselect'
 
 describe('okaselect', () => {
-  it('hello', () => {
-    expect(hello()).toBe('hello')
+  describe('useSelectable', () => {
+    const items = {
+      a: { id: 'a' },
+      b: { id: 'b' },
+      c: { id: 'c' },
+    }
+
+    describe('when shift is false', () => {
+      it('should replace the id', () => {
+        const target = useSelectable(() => items)
+        expect(target.getSelectedItems()).toEqual([])
+        expect(target.getLastSelectedId()).toBe(undefined)
+        target.select('a')
+        expect(target.getSelectedItems()).toEqual([{ id: 'a' }])
+        expect(target.getLastSelectedId()).toBe('a')
+      })
+    })
+
+    describe('when shift is true', () => {
+      it('should add the id if it has not been saved', () => {
+        const target = useSelectable(() => items)
+        target.select('a', true)
+        expect(target.getSelectedItems()).toEqual([{ id: 'a' }])
+        target.select('c', true)
+        expect(target.getSelectedItems()).toEqual([{ id: 'a' }, { id: 'c' }])
+        expect(target.getSelectedIds()).toEqual(['a', 'c'])
+      })
+      it('should remove the id if it has been saved', () => {
+        const target = useSelectable(() => items)
+        target.select('a', true)
+        target.select('c', true)
+        target.select('a', true)
+        expect(target.getSelectedItems()).toEqual([{ id: 'c' }])
+      })
+    })
   })
 })
--- src/okaselect.ts
+++ src/okaselect.ts
@@ -1,1 +1,46 @@
-export const hello = () => 'hello'
+export type Identity = string | number
+export type Items<T> = { [id: Identity]: T }
+
+type SelectedMap = Map<Identity, true>
+
+export function useSelectable<T>(getItems: () => Items<T>) {
+  // this map save the order of selections
+  const selectedIds: SelectedMap = new Map()
+
+  // returned array keeps the order of selections
+  function getSelectedIds(): Identity[] {
+    return Array.from(selectedIds.keys())
+  }
+  function getSelectedItems(): T[] {
+    const items = getItems()
+    return Array.from(selectedIds.keys()).map((identity) => items[identity])
+  }
+
+  function getLastSelectedId(): Identity | undefined {
+    const keys = Array.from(selectedIds.keys())
+    return keys.length === 0 ? undefined : keys[keys.length - 1]
+  }
+
+  function select(id: Identity, shift = false): void {
+    execSelect(selectedIds, id, shift)
+  }
+
+  return {
+    getSelectedItems,
+    getSelectedIds,
+    getLastSelectedId,
+    select,
+  }
+}
+
+function execSelect(map: SelectedMap, id: Identity, shift = false): void {
+  if (!shift) {
+    map.set(id, true)
+  } else {
+    if (map.has(id)) {
+      map.delete(id)
+    } else {
+      map.set(id, true)
+    }
+  }
+}
